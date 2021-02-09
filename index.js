529cdff4b1e6ef24a7a0283afccfd8a4b42230df
Borrison <borissoto@gmail.com>
2021-02-08 23:57:45 -0400

    files get required once

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const counterObject = require('./myscript.js');
 console.log(counterObject.getCounter());
 counterObject.incrementCounter();
 console.log(counterObject.getCounter());
+
+const newCounterObject = require('./myscript');
+
+console.log(newCounterObject.getCounter());
