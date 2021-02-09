a99698d888a44a821144f44b7a08308e896c1fea
Borrison <borissoto@gmail.com>
2021-02-09 01:28:19 -0400

    running a node program as an executable and link the project

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const { getCounter } = require('./myscript');
+#!/usr/bin/env node
 
-const counterObject = require('./myscript.js');
+const fs = require('fs');
 
-console.log(counterObject.getCounter());
-counterObject.incrementCounter();
-console.log(counterObject.getCounter());
+fs.readdir(process.cwd(), (err, filenames) => {
+  // EITHER
+  // err === an error object, wihc means something went wrong
+  // OR
+  // err === null, which menas everything is OK
 
-const newCounterObject = require('./myscript');
+  if (err) {
+    //   error handling code here
+    console.log(err);
+  }
 
-console.log(newCounterObject.getCounter());
+  console.log(filenames);
+});
+
+// const counterObject = require('./myscript.js');
+
+// console.log(counterObject.getCounter());
+// counterObject.incrementCounter();
+// console.log(counterObject.getCounter());
+
+// const newCounterObject = require('./myscript.js');
+
+// console.log(newCounterObject.getCounter());
