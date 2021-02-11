19733ce609caf9bc310884d8ffabaa15d7a811ae
Borrison <borissoto@gmail.com>
2021-02-11 00:23:52 -0400

    callback - based function using promises

--- index.js
+++ index.js
@@ -2,7 +2,15 @@
 
 const fs = require('fs');
 
-fs.readdir(process.cwd(), (err, filenames) => {
+// OPTION CODE #2  Method #2
+// const util = require('util');
+// const lstat = util.promisify(fs.lstat);
+// OPTION CODE #2  Method #2
+// OPTION CODE #2 Method#3
+// const { lstat } = fs.promises;
+// OPTION CODE #2 Method #3
+
+fs.readdir(process.cwd(), async (err, filenames) => {
   // EITHER
   // err === an error object, wihc means something went wrong
   // OR
@@ -14,29 +22,53 @@ fs.readdir(process.cwd(), (err, filenames) => {
   }
   // console.log(filenames);
 
-  // BAD CODE HERE!
-  const allStats = Array(filenames.length).fill(null);
+  // OPTION CODE #2 Method #1#2#3
   for (let filename of filenames) {
-    const index = filenames.indexOf(filename);
-    fs.lstat(filename, (err, stats) => {
-      if (err) {
-        console.log(err);
-      }
-      allStats[index] = stats;
-
-      const ready = allStats.every((stats) => {
-        return stats;
-      });
-
-      if (ready) {
-        allStats.forEach((stats, index) => {
-          console.log(filenames[index], stats.isFile());
-        });
-      }
-    });
+    try {
+      const stats = await lstat(filename);
+      console.log(filename, stats.isFile());
+    } catch (err) {
+      console.log(err);
+    }
   }
-  // BAD CODE COMPLETE
+  // OPTION CODE #2 Method #1#2#3
+
+  // OPTION CODE HERE!
+  // const allStats = Array(filenames.length).fill(null);
+  // for (let filename of filenames) {
+  //   const index = filenames.indexOf(filename);
+  //   fs.lstat(filename, (err, stats) => {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //     allStats[index] = stats;
+
+  //     const ready = allStats.every((stats) => {
+  //       return stats;
+  //     });
+
+  //     if (ready) {
+  //       allStats.forEach((stats, index) => {
+  //         console.log(filenames[index], stats.isFile());
+  //       });
+  //     }
+  //   });
+  // }
+  // OPTION CODE COMPLETE
 });
+
+// OPTION CODE #2 Method #1
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(stats);
+//     });
+//   });
+// };
+// COMPLETE OPTION CODE #2
 
 // const counterObject = require('./myscript.js');
 
