8f8d2753678129a7fa22b46aaafc0fbb3c675545
Borrison <borissoto@gmail.com>
2021-02-10 23:45:55 -0400

    checking if it is a folder

--- index.js
+++ index.js
@@ -12,16 +12,30 @@ fs.readdir(process.cwd(), (err, filenames) => {
     //   error handling code here
     console.log(err);
   }
+  // console.log(filenames);
 
   // BAD CODE HERE!
+  const allStats = Array(filenames.length).fill(null);
   for (let filename of filenames) {
+    const index = filenames.indexOf(filename);
     fs.lstat(filename, (err, stats) => {
       if (err) {
         console.log(err);
       }
-      console.log(filename, stats.isFile());
+      allStats[index] = stats;
+
+      const ready = allStats.every((stats) => {
+        return stats;
+      });
+
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(filenames[index], stats.isFile());
+        });
+      }
     });
   }
+  // BAD CODE COMPLETE
 });
 
 // const counterObject = require('./myscript.js');
